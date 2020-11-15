bccdf94f2a69d78b1b4ef90d337316e6253913ba
bibumun-beebz <bibimunbeebz@gmail.com>
2020-11-15 22:06:14 +0000

    Day 19 decision trees

---
+++ Day19/day19.py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+from sklearn import datasets
+from sklearn import metrics
+from sklearn.naive_bayes import GaussianNB
+
+def make_meshgrid(x, y, h=0.05):
+    x_min, x_max = x.min() - 1, x.max() + 1
+    y_min, y_max = y.min() - 1, y.max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    return xx, yy
+
+def plot_contours(ax, clf, xx, yy, **params):
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    out = ax.contourf(xx, yy, Z, **params)
+    return out
+
+
+def calcColour(label):
+    if label == 0:
+        return 'red'
+    if label == 1:
+        return 'blue'
+    if label == 2:
+        return 'green'
+    return 'black'  
+    
+from sklearn.datasets import load_iris
+iris = load_iris()
+
+from sklearn import decomposition 
+pca = decomposition.PCA(n_components=2)
+pca.fit(iris.data)
+X = pca.transform(iris.data)
+
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, iris.target, test_size=0.3,random_state=109)
+color= [calcColour(l) for l in iris.target]
+xx, yy = make_meshgrid(X[:,0], X[:,1])
+from sklearn.ensemble import BaggingClassifier
+
+model = BaggingClassifier(base_estimator=DecisionTreeClassifier(splitter='random'), n_estimators=20, random_state=0).fit(X_train, y_train)
+
+#plt.style.use('dark_background')
+#plt.rcParams['text.color'] = 'black'
+#fig, ax = plt.subplots(figsize=(10, 10), facecolor='k')
+#tree.plot_tree(model, rotate=True, ax=ax);
+#plt.show()
+print("Accuracy:",metrics.accuracy_score(y_test, model.predict(X_test)))
+
+plot_contours(plt, model, xx, yy, cmap=plt.cm.coolwarm, alpha=0.8)
+plt.scatter(X[:,0], X[:,1], color=color)
+plt.show()
+
